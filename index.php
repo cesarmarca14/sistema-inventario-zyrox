04e799eb280a1d2f5f6017382fe541df24f7e067
cesarlurin <cesarmarca@iestplurin.edu.pe>
2026-06-19 17:16:23 -0500

    g

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+
+<section class="cards-grid">
+    <div class="card">
+        <h3>Total Productos</h3>
+        <p>120</p>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
